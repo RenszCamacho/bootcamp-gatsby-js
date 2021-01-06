47c5d98e7e8c9c94b7a19ecf76024460369b321d
Renszo Camacho <varsovia84@gmail.com>
2021-01-06 01:31:52 +0100

    Styling Gatsby with styled-components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background: red;
+`
 
 function Index() {
   return (
     <Layout>
-      <h1>Hello</h1>
-      <p>I am Renszo</p>
-      <p>
-        Need a developer? <Link to="/contact">Contactame Me</Link>
-      </p>
+      <Container>
+        <h1>Hello</h1>
+        <p>I am Renszo</p>
+        <p>
+          Need a developer? <Link to="/contact">Contactame Me</Link>
+        </p>
+      </Container>
     </Layout>
   )
 }
