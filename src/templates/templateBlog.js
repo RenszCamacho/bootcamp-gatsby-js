eaa53d68d7af4c1e202fab49236fa7f4e488acac
Renszo Camacho <varsovia84@gmail.com>
2021-01-08 17:18:58 +0100

    Generated the blog templates.

---
+++ src/templates/templateBlog.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function templateBlog() {
+  return <Layout>This a the blog template</Layout>
+}
+
+export default templateBlog
