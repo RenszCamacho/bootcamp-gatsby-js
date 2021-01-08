f78c125c626865d7ca659408f7821e9382357c36
Renszo Camacho <varsovia84@gmail.com>
2021-01-08 17:13:32 +0100

    Generate a slug for each post.

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const path = require("path")
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  // Transform the new node here and create a new node or
+  // create a new node field.
+  if (node.internal.type === "Mdx") {
+    // console.log(JSON.stringify(node, undefined, 4))
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
