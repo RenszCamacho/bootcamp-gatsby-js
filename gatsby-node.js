1d3b9fd5d5ef95eced00711156f9cba39c4edc3e
Renszo Camacho <varsovia84@gmail.com>
2021-01-11 12:45:29 +0100

    Fixing the 'Dinamically Generating Pages'

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,31 +16,51 @@ exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-// module.exports.createPages = async ({ graphql, actions }) => {
-//   const { createPages } = actions
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`./src/templates/blog.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(
+    `
+      query {
+        allMdx {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `,
+    { limit: 1000 }
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
 
-//   // 1.Get path to template
-//   const blogTemplate = path.resolve("./src/templates/templateBlog.js")
-//   const res = await graphql`
-//     query {
-//       allMdx {
-//         edges {
-//           node {
-//             fields {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `
-//   res.allMdx.edges.map(edge => {
-//     createPages({
-//       components: blogTemplate,
-//       path: `/blog/${edges.node.fields.slug}`,
-//       context: {
-//         slug: edge.node.fields.slug,
-//       },
-//     })
-//   })
-// }
+    // Create blog post pages.
+    result.data.allMdx.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `/blog/${edge.node.fields.slug}`,
+        component: blogPostTemplate,
+        context: {
+          slug: edge.node.fields.slug,
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+        },
+      })
+    })
+  })
+}
